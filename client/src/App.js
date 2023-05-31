a4c729c3ad1df8d976108b2e04a8c75e14b6a151
fasilkp <fasilkp314@gmail.com>
2023-05-31 19:38:19 +0530

    gauth integration

--- client/src/App.js
+++ client/src/App.js
@@ -9,20 +9,6 @@ import "./App.css";
 function App() {
 	const [user, setUser] = useState(null);
 
-	const getUser = async () => {
-		try {
-			const url = `${process.env.REACT_APP_API_URL}/auth/login/success`;
-			const { data } = await axios.get(url, { withCredentials: true });
-			setUser(data.user._json);
-		} catch (err) {
-			console.log(err);
-		}
-	};
-
-	useEffect(() => {
-		getUser();
-	}, []);
-
 	return (
 		<div className="container">
 			<Routes>
--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -1,12 +1,11 @@
 import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
+import axios from "axios";
+import GoogleLogin from 'react-google-login';
 
 function Login() {
-	const googleAuth = () => {
-		window.open(
-			`${process.env.REACT_APP_API_URL}/auth/google/callback`,
-			"_self"
-		);
+	const googleAuth = async (response) => {
+		console.log(response)
 	};
 	return (
 		<div className={styles.container}>
@@ -16,18 +15,25 @@ function Login() {
 					<img className={styles.img} src="./images/login.jpg" alt="login" />
 				</div>
 				<div className={styles.right}>
-					<h2 className={styles.from_heading}>Members Log in</h2>
+					{/* <h2 className={styles.from_heading}>Members Log in</h2>
 					<input type="text" className={styles.input} placeholder="Email" />
 					<input type="text" className={styles.input} placeholder="Password" />
 					<button className={styles.btn}>Log In</button>
-					<p className={styles.text}>or</p>
-					<button className={styles.google_btn} onClick={googleAuth}>
-						<img src="./images/google.png" alt="google icon" />
-						<span>Sing in with Google</span>
-					</button>
-					<p className={styles.text}>
+					<p className={styles.text}>or</p> */}
+					{/* <button className={styles.google_btn} >
+						<img src="./images/google.png" alt="google icon" /> */}
+						{/* <span>Sign in with Google</span> */}
+						<GoogleLogin
+							clientId="572510792166-vpf7ki1vmt5t7u4er1afdsgn7oe1l1l9.apps.googleusercontent.com"
+							buttonText="Login with Google"
+							onSuccess={googleAuth}
+							onFailure={googleAuth}
+							cookiePolicy={'single_host_origin'}
+						/>
+					{/* </button> */}
+					{/* <p className={styles.text}>
 						New Here ? <Link to="/signup">Sing Up</Link>
-					</p>
+					</p> */}
 				</div>
 			</div>
 		</div>
